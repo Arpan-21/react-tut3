60f3c337a6a0db352b8317ef5dae966b78272564
ARPAN JAIN <arpan.jain.ug20@nsut.ac.in>
2023-04-30 00:53:26 +0530

    first commti

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,41 @@
+import './SeasonsDisplay.css';
+// import react from 'react';
+// import Spinner from './Spinner';
+
+
+const seasonConfig = {
+    summer :{
+        text : `Let's hit the beach`,
+        iconName : 'sun'
+    },
+    winter :{
+        text : `It's too cold outside`,
+        iconName : 'snowflake'
+    }
+}
+
+const getSeason = (lat,month)=>{
+    if(month > 2 && month <9){
+        return lat > 0 ? 'summer' : 'winter';
+    }
+    else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props)=>{
+    // console.log(props);
+    // console.log(season);
+    const season =getSeason(props.lat , new Date().getMonth());
+    const {text , iconName} = seasonConfig[season];
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>{text}. <br/>
+            <h3>Current time is : {props.time}</h3></h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    )
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonsDisplay';
+import Spinner from './Spinner';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// stateless function component
+// const App = () =>{
+//     navigator.geolocation.getCurrentPosition(
+//         position=>console.log(position),
+//         err=>console.log(err) 
+//     );
+//     return(
+//         <div>Hi there!!</div>
+//     );
+// };
+class App extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { lat: null, errorMessage: '' };
+    //     // navigator.geolocation.getCurrentPosition(
+    //         //     // position=>console.log(position),
+    //         //     position => {
+    //             //         this.setState({ lat: position.coords.latitude });
+    //             //     },
+    //             //     // err => console.log(err)
+    //             //     err => {
+    //                 //         this.setState({ errorMesage: err.message })
+    //                 //     }
+    //                 // );
+    // }
+    // componentDidMount(){
+    //     console.log('My component was rendered to the screen.');
+    // }
+    // componentDidUpdate(){
+    //     console.log('My component was just updated- it rerendered.');
+    // }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //initializaiton
+    state = { lat: null, errorMessage: '', time:'' };
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMesage: err.message })
+        );
+        setInterval(()=> {
+            this.setState({ time : new Date().toLocaleTimeString()})
+        },1000)
+    }
+    render() {
+        // return (
+        //     <div>
+        //     Latitude:{this.state.lat}
+        //     <br/>
+        //     Error: {this.state.errorMesage}
+        //     </div>
+        // )
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return (
+                <SeasonDisplay lat={this.state.lat} time={this.state.time} />
+            );
+        }
+        return <Spinner message="Please accept the location request"/>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
